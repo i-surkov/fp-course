216976091387b9301d4eab051ed748fc5fafb61d
Ilya Surkov <ilyas9406@yandex.ru>
2020-11-23 10:10:43 -0600

    Complete excercise 'FileIO'

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
@@ -85,46 +86,48 @@ printFile ::
   FilePath
   -> Chars
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+printFile path content = putStrLn $ "File " ++ path ++ "\nContent:\n" ++ content
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+printFiles = myMapM_ (uncurry printFile)
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile path = (path,) <$> readFile path
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+getFiles = myMapM getFile
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@, @lines@, and @printFiles@.
 run ::
   FilePath
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run path = do
+  filenames <- lines . snd <$> getFile path
+  printFiles <=< getFiles $ filenames
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
-main =
-  error "todo: Course.FileIO#main"
+main = getArgs >>= myMapM_ run
+
+myMapM :: Applicative k => (a -> k b) -> List a -> k (List b)
+myMapM f = sequence . (<$>) f
+
+myMapM_ :: Applicative k => (a1 -> k a2) -> List a1 -> k ()
+myMapM_ f = void . sequence . (<$>) f
 
 ----
 
